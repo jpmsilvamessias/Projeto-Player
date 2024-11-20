94c1d508e80297c01a06126bcf08e478805fa14a
GuilhermeVicenteFigueira <figueira.gui1@gmail.com>
2024-11-20 20:09:58 -0300

    ajeitação2

--- class/Defesa.php
+++ class/Defesa.php
@@ -4,7 +4,7 @@ require_once('Item.php');
 
 class Defesa extends Item{
     
-    public function __construct(string $nome, int $tamanho, string $class){
+    public function __construct(string $nome, int $tamanho, string $classe){
         parents::__construct($nome, $tamanho, $classe);
     }
 }
--- class/Magia.php
+++ class/Magia.php
@@ -3,5 +3,8 @@
 require_once('Item.php');
 
 class Magia extends Item {
+    public function __construc(string $nome, int $tamanho, string $classe){
+        parents::__cosntruct($nome, $tamanho, $classe);
+    }
     
 }
