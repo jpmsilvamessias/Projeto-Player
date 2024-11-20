57df87a3cb4f83ee02001df9b155667a2720f631
GuilhermeVicenteFigueira <figueira.gui1@gmail.com>
2024-11-20 20:07:06 -0300

    ajeitação1

--- class/Defesa.php
+++ class/Defesa.php
@@ -5,6 +5,6 @@ require_once('Item.php');
 class Defesa extends Item{
     
     public function __construct(string $nome, int $tamanho, string $class){
-        parents::__construct($nome, $tamanho, $classe)
+        parents::__construct($nome, $tamanho, $classe);
     }
 }
