eba7566a467b4ccc4a95b4d1e2d40fec3692ecdc
GuilhermeVicenteFigueira <figueira.gui1@gmail.com>
2024-11-24 17:43:55 -0300

    a

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
+
 require_once('./class/Ataque.php');
 require_once('./class/Defesa.php');
 require_once('./class/Inventario.php');
 require_once('./class/Item');
 require_once('./class/Magia');
 require_once('./class/Player');
+
 $item1=new Ataque('Espada',7,'ataque');
 
 $player1= new Player('detah of angel',1);
 $player1->coletarItem($item1);
 $player->adcionar($item);
-a
 
