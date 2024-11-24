ff85d4fabf61aed15f929fa503ba06b5efdb49c5
jpmsilvamessias <joaopedr901673@gmail.com>
2024-11-24 17:36:49 -0300

    agora s

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <?php
-
-<<<<<<< HEAD
-$item1=new Ataque('Espada',7,'ataque');
-
-$player1= new Player('detah of angel',1);
-$player1->coletarItem($item1);
-$player->adcionar($item);
-=======
 require_once('./class/Ataque.php');
 require_once('./class/Defesa.php');
 require_once('./class/Inventario.php');
 require_once('./class/Item');
 require_once('./class/Magia');
 require_once('./class/Player');
->>>>>>> 17e636c49a56edcdb43f4f7adab35ce657bc5899
+$item1=new Ataque('Espada',7,'ataque');
+
+$player1= new Player('detah of angel',1);
+$player1->coletarItem($item1);
+$player->adcionar($item);
+
