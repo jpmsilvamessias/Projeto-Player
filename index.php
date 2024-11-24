b516f9a55fd5f928b4b154de09a0e16fa2ae716d
jpmsilvamessias <joaopedr901673@gmail.com>
2024-11-24 17:42:50 -0300

    mudança

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 require_once('./class/Ataque.php');
 require_once('./class/Defesa.php');
 require_once('./class/Inventario.php');
-require_once('./class/Item');
-require_once('./class/Magia');
-require_once('./class/Player');
+require_once('./class/Item.php');
+require_once('./class/Magia.php');
+require_once('./class/Player.php');
 $item1=new Ataque('Espada',7,'ataque');
 
 $player1= new Player('detah of angel',1);
 $player1->coletarItem($item1);
 $player->adcionar($item);
-a
-
