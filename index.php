17e636c49a56edcdb43f4f7adab35ce657bc5899
GuilhermeVicenteFigueira <figueira.gui1@gmail.com>
2024-11-24 17:32:05 -0300

    Index.pt1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once('./class/Ataque.php');
+require_once('./class/Defesa.php');
+require_once('./class/Inventario.php');
+require_once('./class/Item');
+require_once('./class/Magia');
+require_once('./class/Player');
