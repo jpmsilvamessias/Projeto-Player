160a2cda2112d8814682e9da8b7c5f48c8bdf055
jpmsilvamessias <joaopedr901673@gmail.com>
2024-11-27 20:52:00 -0300

    Update Defesa.php

--- class/Defesa.php
+++ class/Defesa.php
@@ -4,7 +4,7 @@ require_once('Item.php');
 
 class Defesa extends Item{
     
-    public function __construct(string $nome, int $tamanho, string $classe){
-        parent::__construct($nome, $tamanho, $classe);
+    public function __construct(string $nome){
+        parent::__construct($nome, 4,'defesa');
     }
 }
