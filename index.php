050c4a637be19c4d26832ab1b31740092db209aa
GuilhermeVicenteFigueira <figueira.gui1@gmail.com>
2024-11-27 16:36:22 -0300

    Index final

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ echo "inventario atual do jogador {$player1->getNickname()}<br>";
 echo "capacidade maxima atual{$player1->getInventario()->getCapacidadeMaxima()}<br>";
 echo "capacidade livre atual{$player1->getInventario()->capacidadeLivre()}<br>";
 echo "itens no inventario<br>";
+
 foreach ($player1->getInventario()->listar() as $item) {
     echo " Nome {$item->getNome()}  Tamanho: {$item->getTamanho()}<br>";
 }
@@ -27,17 +28,19 @@ echo "jogador {$player1->getNickName()}, subiu de nivel para {$player1->getNivel
 
 echo "Nova capacidade Maxima{$player1->getInventario()->getCapacidadeMaxima()}<br>";
 
- echo'<br>';
+echo'<br>';
 
 $player2= new Player("link",1);
 $item2= new Ataque('Master sword',7,'ataque');
 $item3= new Magia('Cajado' ,2 ,'magia');
 $item4= new Defesa('Escudo',4,'Defesa');
 $item5= new Ataque('Machado',7,'ataque');
-$item6= new Magia('Bola de fogop',2,'magia');
+$item6= new Magia('Bola de fogo',2,'magia');
+
 echo "<br>";
-$item7= new Ataque('espada gigante',7,'ataque');
-$item8= new Magia ('oraculo',2,'magia');
+
+$item7= new Ataque('Espada gigante',7,'ataque');
+$item8= new Magia ('Oraculo',2,'magia');
 
 $player2->getInventario()->adicionar($item2);
 $player2->getInventario()->adicionar($item3);
@@ -47,10 +50,11 @@ $player2->getInventario()->adicionar($item6);
 $player2->getInventario()->adicionar($item7);
 $player2->getInventario()->adicionar($item8);
  
-echo "inventario atual do jogador {$player2->getNickname()}<br>";
-echo "capacidade maxima atual{$player2->getInventario()->getCapacidadeMaxima()}<br>";
-echo "capacidade livre atual {$player2->getInventario()->capacidadeLivre()}<br>";
-echo "itens no inventario<br>";
+echo "Inventario atual do jogador {$player2->getNickname()}<br>";
+echo "Capacidade maxima atual{$player2->getInventario()->getCapacidadeMaxima()}<br>";
+echo "Capacidade livre atual {$player2->getInventario()->capacidadeLivre()}<br>";
+echo "Itens no inventario<br>";
+
 foreach ($player2->getInventario()->listar() as $item) {
     echo " Nome {$item->getNome()}  Tamanho: {$item->getTamanho()}<br>";
 }
